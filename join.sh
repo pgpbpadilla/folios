75b97180952fa76a740047864ef9140c00fa165f
Pablo Padilla <pgpb.padilla@gmail.com>
2016-07-07 22:04:31 -0700

    chg: dev: Simplify functions and make sure to use BASH expansion when passing list of strings to functions

--- join.sh
+++ join.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-function print {
-    for line in $1
-    do
-        echo $line
-    done
-}
-
 function get_column {
     local COL_NUM=$2
     local FILE=$1
 
-    print $(cat $FILE | cut -d',' -f$COL_NUM)
+    cat $FILE | cut -d',' -f$COL_NUM
 }
 
 function list2file {
@@ -32,10 +25,40 @@ function get_intersection {
     local COL1=$1
     local COL2=$2
     
-    local TMPF1=$(list2file $COL1)
-    local TMPF2=$(list2file $COL2)
+    local TMPF1=$(list2file "${COL1}")
+    local TMPF2=$(list2file "${COL2}")
 
-    local INTERSECTION=$(comm -12 $TMPF1 $TMPF2)
-
-    print $INTERSECTION
+    comm -12 $TMPF1 $TMPF2
 }
+
+function lookup {
+    local FILE=$1
+    local FOLIO=$2
+    local COL_NUMS=$3
+
+    grep $FOLIO $FILE | cut -d',' -f$COL_NUMS
+}
+
+
+## Main program
+
+ALL_FILE=$1
+MONTH_FILE=$2
+
+ALL_FOLIOS=$(get_column "${ALL_FILE}" 1)
+MONTH_FOLIOS=$(get_column "${MONTH_FILE}" 2)
+
+FOLIOS=$(get_intersection "${ALL_FOLIOS}" "${MONTH_FOLIOS}")
+
+
+for folio in $FOLIOS
+do
+    echo "Extract col2 for $folio from file: ${ALL_FILE}"
+    lookup $ALL_FILE ${folio} 2
+done
+
+for folio in $FOLIOS
+do
+    echo "Extract col2 and col3 for $folio from file: ${MONTH_FILE}"
+    lookup $MONTH_FILE ${folio} 1,3
+done
